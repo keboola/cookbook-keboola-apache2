851be2627d53888facba3f418c11d33981f3cb39
Martin Halamíček <martin@halamicek.cz>
2015-11-13 07:26:48 +0100

    mpm prefork to defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,11 @@ default['apache']['default_modules'] = %w[
   status alias auth_basic authn_core authn_file authz_core authz_groupfile
   authz_host authz_user autoindex dir env mime negotiation setenvif logio log_config unixd socache_shmcb filter
 ]
+
+
+default['apache']['prefork']['startservers']        = 5
+default['apache']['prefork']['minspareservers']     = 5
+default['apache']['prefork']['maxspareservers']     = 10
+default['apache']['prefork']['serverlimit']         = 256
+default['apache']['prefork']['maxrequestworkers']   = 256
+default['apache']['prefork']['maxconnectionsperchild'] = 10_000
