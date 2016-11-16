3e54d0530cb22b2853906c5427b4a255714c77ca
Martin Halamíček <martin@halamicek.cz>
2016-11-16 10:08:20 +0100

    ssl removed

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,60 +28,10 @@ r.service_name('httpd')
 apache_default_template = resources(:template => "apache2.conf")
 apache_default_template.cookbook "keboola-apache2"
 
-unless node['apache']['listen_ports'].include?('443')
-  node.set['apache']['listen_ports'] = node['apache']['listen_ports'] + ['443']
-end
-
-if platform_family?('rhel', 'fedora', 'suse')
-  package 'mod24_ssl' do
-    notifies :run, 'execute[generate-module-list]', :immediately
-  end
-
-  file "#{node['apache']['dir']}/conf.d/ssl.conf" do
-    action :delete
-    backup false
-  end
-end
-
-template 'ssl_ports.conf' do
-  path      "#{node['apache']['dir']}/ports.conf"
-  source    'ports.conf.erb'
-  mode      '0644'
-  cookbook  'apache2'
-  notifies  :restart, 'service[apache2]'
-end
-
-apache_module 'ssl' do
-  conf true
-  cookbook 'apache2'
-end
-
 
 apache_sysconfig_template = resources(:template => "/etc/sysconfig/#{node['apache']['package']}")
 apache_sysconfig_template.cookbook "keboola-apache2"
 
-
-execute "download certificaties from s3" do
-  command "aws s3 cp s3://#{node['keboola-apache']['certificates-bucket']}/certificates/ssl-keboola.com.tar.gz /tmp/ssl-keboola.com.tar.gz --region #{node['aws']['region']}"
-  environment(
-    'AWS_ACCESS_KEY_ID' => node['aws']['aws_access_key_id'],
-    'AWS_SECRET_ACCESS_KEY' => node['aws']['aws_secret_access_key']
-  )
-end
-
-
-directory "#{node['apache']['dir']}/ssl" do
-  owner "root"
-  group "root"
-  mode 00644
-  action :create
-end
-
-execute "extract-certificates" do
-  command "tar --strip 1 -C #{node['apache']['dir']}/ssl -xf  /tmp/ssl-keboola.com.tar.gz"
-  user "root"
-  group "root"
-end
 
 ## PHP module
 file "#{node['apache']['dir']}/conf.d/php.conf" do
